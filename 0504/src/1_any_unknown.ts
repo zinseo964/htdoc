12e78ea794ec82113195fc7f048ae930c6d8797f
zinger <wlstj9964@gmail.com>
2022-05-09 13:48:57 +0900

    0504 typescript example

--- 0504/src/1_any_unknown.ts
+++ 0504/src/1_any_unknown.ts
@@ -2,24 +2,24 @@
  * any & unknown type
  */
 
- let slimeOne: any = 1;
- slimeOne = "string";
- slimeOne = true;
- slimeOne = null;
- slimeOne = undefined;
- slimeOne = [1, 2, 3];
- slimeOne = () => {};
- slimeOne = { key: "something " };
- slimeOne.key;
- 
- let slimeTwo: unknown = 1;
- slimeTwo = "string";
- slimeTwo = true;
- slimeTwo = null;
- slimeTwo = undefined;
- slimeTwo = [1, 2, 3];
- slimeTwo = () => {};
- slimeTwo = { key: "something" };
- slimeTwo.key;
- 
- (<{ key: string }>slimeTwo).key;
+let slimeOne: any = 1;
+slimeOne = "string";
+slimeOne = true;
+slimeOne = null;
+slimeOne = undefined;
+slimeOne = [1, 2, 3];
+slimeOne = () => {};
+slimeOne = { key: "something " };
+slimeOne.key;
+
+let slimeTwo: unknown = 1;
+slimeTwo = "string";
+slimeTwo = true;
+slimeTwo = null;
+slimeTwo = undefined;
+slimeTwo = [1, 2, 3];
+slimeTwo = () => {};
+slimeTwo = { key: "something" };
+// slimeTwo.key;
+
+(<{ key: string }>slimeTwo).key;
